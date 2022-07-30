686ce344358e96df4e308d95cd2e29b09f7cac98
vishaljoshy18 <1812-vishal@protonmail.com>
2022-07-30 13:59:32 +0530

    Change array to vector.

--- parameterization/basic.cpp
+++ parameterization/basic.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
-#include <memory>
+#include <vector>
 
-void BSort(int *arr, int length)
+std::vector<int> BSort(std::vector<int> arr, int length)
 {
-    if (arr == nullptr)
-        return;
-
+    int temp;
     for (int i = 0; i < length; i++)
     {
         for (int j = 0; j < length - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
-                int temp = arr[j + 1];
+                temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
             }
         }
     }
+    return arr;
 }
 
 int main()
 {
-    int arr[5] = {5, 3, 2, 4, 1};
-    BSort(arr, 5);
+    std::vector<int> arr = {5, 3, 2, 4, 1};
 
-    for (int i : arr)
+    std::vector sorted = BSort(arr, 5);
+
+    for (int i : sorted)
     {
         std::cout << i;
     }
